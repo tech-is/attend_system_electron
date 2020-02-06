9cb46056ce1b62dc5c19a88efa1746eebab8ed58
Hironobu-Mori <cipher_galm01@outlook.jp>
2020-02-06 16:00:46 +0900

    #1 レビュー箇所修正

--- src/assets/js/req.js
+++ src/assets/js/req.js
@@ -1,7 +1,6 @@
 let inputString = '';
-// let studentID = null;
 const STORAGE_KEY = 'techis';
-const tableElem = document.getElementById('tbody');
+const TABLE_ELEM = document.getElementById('tbody');
 const KEYOBJ = {
     48: 0,
     49: 1,
@@ -14,12 +13,12 @@ const KEYOBJ = {
     56: 8,
     57: 9
 };
-const logObj = JSON.parse(sessionStorage.getItem(STORAGE_KEY) || '[]');
+KEYCODE_ENTER = 13
+const LOGOBJ = JSON.parse(sessionStorage.getItem(STORAGE_KEY) || '[]');
 
 function onPress() {
     let key = event.keyCode;
-    if (key === 13) {
-        // console.log(inputString);
+    if (key === KEYCODE_ENTER) {
         let name = null;
         let studentId = null;
         let barcode = Number(inputString);
@@ -34,26 +33,24 @@ function onPress() {
                 .then(recordID => {
                     if (recordID > 0) {
                         update_kintone_record(recordID).then(() => {
-                            logObj.unshift({
+                            LOGOBJ.unshift({
                                 'name': name,
                                 'created_at': getNowDatetime(),
                                 'status': '退席済'
                             })
-                            sessionStorage.setItem(STORAGE_KEY, JSON.stringify(logObj));
-                            // localStorage.setItem(STORAGE_KEY, JSON.stringify(logObj));
-                            generateRow(tableElem);
+                            sessionStorage.setItem(STORAGE_KEY, JSON.stringify(LOGOBJ));
+                            generateRow(TABLE_ELEM);
                             SuccessAlert(name);
                         });
                     } else {
                         insert_kintone_record(studentId).then(() => {
-                            logObj.unshift({
+                            LOGOBJ.unshift({
                                 'name': name,
                                 'created_at': getNowDatetime(),
                                 'status': '出席中'
                             })
-                            sessionStorage.setItem(STORAGE_KEY, JSON.stringify(logObj));
-                            // localStorage.setItem(STORAGE_KEY, JSON.stringify(logObj));
-                            generateRow(tableElem);
+                            sessionStorage.setItem(STORAGE_KEY, JSON.stringify(LOGOBJ));
+                            generateRow(TABLE_ELEM);
                             SuccessAlert(name, true);
                         });
                     }
@@ -66,7 +63,9 @@ function onPress() {
             errorAlert('バーコードが入力されていません');
         }
     } else {
-        key in KEYOBJ ? inputString += KEYOBJ[key] : false;
+        if (key in KEYOBJ) {
+            inputString += KEYOBJ[key]
+        }
     }
 }
 
@@ -81,7 +80,7 @@ function generateRow() {
                 tbody += `<tr><td>${value.name}</td><td>${value.created_at}</td><td>${value.status}</td></tr>`;
             }
         })
-        tableElem.innerHTML = tbody;
+        TABLE_ELEM.innerHTML = tbody;
         return true;
     } else {
         return false;
@@ -108,76 +107,76 @@ function errorAlert(text) {
     })
 }
 
-function getNowYMD(day = 0) {
+function getYMD(add = false) {
     let dt = new Date();
-    day > 0 ? dt.setDate(dt.getDate() + 1) : false;
-    let y = dt.getFullYear();
-    let m = ("00" + (dt.getMonth() + 1)).slice(-2);
-    let d = ("00" + (dt.getDate())).slice(-2);
-    let time = 'T00:00:00' + '\+0900';
-    return y + '-' + m + '-' + d + time;
+    if (add) {
+        dt.setDate(dt.getDate() + 1);
+    }
+    return dt.toISOString().substr(0, 10);
 }
 
 function getNowDatetime() {
-    let dt = new Date();
-    let y = dt.getFullYear();
-    let m = ("00" + (dt.getMonth() + 1)).slice(-2);
-    let d = ("00" + (dt.getDate())).slice(-2);
-    let h = ("00" + dt.getHours()).slice(-2);
-    let i = ("00" + dt.getMinutes()).slice(-2);
-    let s = ("00" + dt.getSeconds()).slice(-2);
-    let time = 'T' + h + ':' + i + ':' + s + '\+0900';
-    return y + '-' + m + '-' + d + time;
+    // return new Date().toISOString().substr(0, 19) + 'Z'
+    return new Date().toISOString();
 }
 
-function fixedEncodeURIComponent(str) {
-    return encodeURIComponent(str).replace(/[!'()*]/g, function (c) {
-        return '%' + c.charCodeAt(0).toString(16);
-    });
+function createQueryString(obj) {
+    let qs = '?';
+    let lastkey = Object.keys(obj).pop();
+    for (let key in obj) {
+        qs += key + '=' + encodeURIComponent(obj[key]) + (lastkey !== key ? '&' : '');
+    }
+    return qs;
 }
 
 async function getStudent(barcode) {
     let url = APICONF.student.url
-    // let conf = APICONF.GET
-    let params = `?app=${APICONF.student.app}&query=${encodeURIComponent("barcode=\"" + barcode + "\"")}&fields[0]=${encodeURIComponent('$id')}&fields[1]=${encodeURIComponent('Name')}&totalCount=true`
+    let params = createQueryString({
+        'app': `${APICONF.student.app}`,
+        'query': 'barcode="' + barcode + '"',
+        'fields[0]': `$id`,
+        'fields[1]': 'Name',
+        'totalCount': 'true'
+    });
     let req = url + params;
     let response = await fetch(req, {
         headers: {
             'X-Cybozu-API-Token': APICONF.token
         }
     })
-    if (await response.ok) {
-        let json = await response.json();
-        if (json.totalCount === '1') {
-            return json['records'][0];
-        } else {
-            throw new Error('バーコードが存在しません');
-        }
-    } else {
+    if (await !response.ok) {
         let text = await response.text();
         throw new Error(`Request failed: ${text}`);
     }
+    let json = await response.json();
+    if (json.totalCount !== '1') {
+        throw new Error('バーコードが存在しません');
+    }
+    return json['records'][0];
 }
 
 async function get_kintone_record(id) {
     let url = APICONF.GET.url
-    let now = getNowYMD();
-    let nextday = getNowYMD(1);
-    let params = `?app=${APICONF.GET.app}&query=${encodeURIComponent("student_id = \"" + Number(id) + "\" and attend_at > \"" + now + "\" and attend_at < \"" + nextday + "\" and attend_status in (\"出席中\")")}
-                &fields[0]=record_id&totalCount=true`
+    let now = getYMD() + 'T00:00:00+0900';
+    let nextday = getYMD(true) + 'T00:00:00+0900';
+    let params = createQueryString({
+        'app': APICONF.GET.app,
+        'query': 'student_id =\"' + Number(id) + '\" and attend_at >\"'
+            + now + '\" and attend_at < \"' + nextday + '\" and attend_status in ("\出席中\")',
+        'fields[0]': 'record_id',
+        'totalCount': true
+    });
     let response = await fetch(url + params, {
         headers: {
             'X-Cybozu-API-Token': APICONF.token
         }
     })
-    if (await response.ok) {
-        let json = await response.json();
-        return json.totalCount === '1' ? Number(json['records'][0]['record_id']['value']) : -1;
-    } else {
-        // throw new Error(`Request failed: ${response.status}`);
+    if (await !response.ok) {
         let text = await response.text();
         throw new Error(`Request failed: ${text}`);
     }
+    let json = await response.json();
+    return json.totalCount === '1' ? Number(json['records'][0]['record_id']['value']) : -1;
 }
 
 async function insert_kintone_record(id) {
@@ -205,12 +204,11 @@ async function insert_kintone_record(id) {
         },
         body: JSON.stringify(data),
     })
-    if (await response.ok) {
-        return true;
-    } else {
+    if (await !response.ok) {
         let text = await response.text();
         throw new Error(`Request failed: ${text}`);
     }
+    return true;
 }
 
 async function update_kintone_record(id) {
@@ -228,8 +226,6 @@ async function update_kintone_record(id) {
             }
         }
     }
-    let header = APICONF.token;
-    header['Content-Type'] = 'application/json';
     let response = await fetch(url, {
         method: 'PUT',
         headers: {
@@ -238,11 +234,9 @@ async function update_kintone_record(id) {
         },
         body: JSON.stringify(data),
     })
-    if (await response.ok) {
-        return true;
-    } else {
-        // throw new Error(`Request failed: ${response.status}`);
+    if (await !response.ok) {
         let text = await response.text();
         throw new Error(`Request failed: ${text}`);
     }
+    return true;
 }
